82781b0e2d0325d9f7f2e780d2dcd2473c1df389
Meraj <razazanOfficial@gmail.com>
2025-09-22 12:46:18 +0330

    6- Header Setup

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,28 +1,21 @@
 import ThemeToggle from "@/components/ThemeToggle";
 import LanguageToggle from "@/components/LanguageToggle";
+import { useTranslations } from "next-intl";
 
 export default function Header() {
+  const t = useTranslations("Header")
   return (
-    <header className="sticky top-0 z-50 bg-surface/80 backdrop-blur border-b border-border">
-      <div className="mx-auto max-w-6xl px-4 py-3 flex items-center justify-between">
-        <div className="flex items-center gap-2">
-          <div className="size-8 rounded-lg bg-brand-800" aria-hidden="true" />
-          <span className="font-semibold text-text">Digital Age Store</span>
-        </div>
-
-        <div className="flex items-center gap-3">
-          <a
-            href="#"
-            className="hidden sm:inline-flex items-center px-3 py-2 rounded-xl border border-border bg-surface text-text hover:opacity-80 transition"
-          >
-            Docs
-          </a>
-
-          <ThemeToggle />
-
-          <LanguageToggle />
-        </div>
-      </div>
+    <header className="flex mt-6">
+      <ThemeToggle />
+      <nav className="w-fit mx-auto bg-transparent rounded-4xl border-2 border-brand-800">
+      <ul className="flex justify-center items-center gap-2 py-2 px-2">
+        <li className="border-[1px] border-brand-500 py-2 px-4 rounded-4xl">{t("HomePage")}</li>
+        <li className="border-[1px] border-brand-500 py-2 px-4 rounded-4xl">{t("Gallery")}</li>
+        <li className="border-[1px] border-brand-500 py-2 px-4 rounded-4xl">{t("AboutUs")}</li>
+        <li className="border-[1px] border-brand-500 py-2 px-4 rounded-4xl">{t("ContactUs")}</li>
+      </ul>
+      </nav>
+      <LanguageToggle />
     </header>
   );
 }
